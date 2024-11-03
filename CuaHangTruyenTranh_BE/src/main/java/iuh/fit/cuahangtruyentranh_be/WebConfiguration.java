04746cdc2dc9500c6ebc48c682dd612c71ea1150
trantrongtin <trantin1973@gmail.com>
2024-11-03 20:25:03 +0700

    tạo WebConfiguration để kết nối FE React

---
+++ CuaHangTruyenTranh_BE/src/main/java/iuh/fit/cuahangtruyentranh_be/WebConfiguration.java
@@ -0,0 +1,15 @@
+package iuh.fit.cuahangtruyentranh_be;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfiguration implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**").allowedMethods("*");
+    }
+}
+
